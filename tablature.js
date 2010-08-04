ddf90700a78cefe73b79d658248fb6ca8171dabd
KHASCHULUU Munkhbayar <xacaapyy@gmail.com>
2010-08-05 05:27:45 +0800

    README.md файл үүсгэлээ. Markdown ашиглаж байгаа анхны туршилт

---
+++ tablature.js
@@ -0,0 +1,7 @@
+function write_tab(s, f)
+{
+	document.pointform.input.value += s;
+	document.pointform.input.value += " ";
+	document.pointform.input.value += f;
+	document.pointform.input.value += "\n";
+}
